13e77c5202372b0ec229bced13a4b356b00a765d
cckaron <cg.workst@gmail.com>
2018-05-15 18:01:56 +0800

    1.串接Cloud Firestore 2.新增店家功能

--- app/src/main/java/tw/com/team13/firebaselogin/Fragment_Profile.java
+++ app/src/main/java/tw/com/team13/firebaselogin/Fragment_Profile.java
@@ -1,13 +1,21 @@
 package tw.com.team13.firebaselogin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class Fragment_Profile extends Fragment {
+    private Button addStore;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+    }
 
 
     @Override
@@ -17,5 +25,18 @@ public class Fragment_Profile extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+        addStore = getView().findViewById(R.id.addStore_button);
+        addStore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getActivity(), addStoreActivity.class); //用getActivity()尋找當前Activity,前往addStoreActivity
+                startActivity(intent);
+            }
+        });
+    }
 
 }
