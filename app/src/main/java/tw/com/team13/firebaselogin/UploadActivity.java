0748642d8b665033c14bca0a8df83dee4aaa78c6
cckaron <cg.workst@gmail.com>
2018-05-22 18:41:29 +0800

    Connect with FireBase Storage for uploading image

---
+++ app/src/main/java/tw/com/team13/firebaselogin/UploadActivity.java
@@ -0,0 +1,259 @@
+package tw.com.team13.firebaselogin;
+
+import android.app.ProgressDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Locale;
+
+public class UploadActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "Storage#MainActivity";
+
+    private static final int RC_TAKE_PICTURE = 101;
+
+    private static final String KEY_FILE_URI = "key_file_uri";
+    private static final String KEY_DOWNLOAD_URL = "key_download_url";
+
+    private BroadcastReceiver mBroadcastReceiver;
+    private ProgressDialog mProgressDialog;
+
+    private Uri mDownloadUrl = null;
+    private Uri mFileUri = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addrestaurant);
+
+        // Click listeners
+        findViewById(R.id.chooseImage_button).setOnClickListener(this);
+        findViewById(R.id.downloadImage_button).setOnClickListener(this);
+
+        // Restore instance state
+        if (savedInstanceState != null) {
+            mFileUri = savedInstanceState.getParcelable(KEY_FILE_URI);
+            mDownloadUrl = savedInstanceState.getParcelable(KEY_DOWNLOAD_URL);
+        }
+        onNewIntent(getIntent());
+
+        // Local broadcast receiver
+        mBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Log.d(TAG, "onReceive:" + intent);
+                hideProgressDialog();
+
+                switch (intent.getAction()) {
+                    case MyDownloadService.DOWNLOAD_COMPLETED:
+                        // Get number of bytes downloaded
+                        long numBytes = intent.getLongExtra(MyDownloadService.EXTRA_BYTES_DOWNLOADED, 0);
+
+                        // Alert success
+                        showMessageDialog(getString(R.string.success), String.format(Locale.getDefault(),
+                                "%d bytes downloaded from %s",
+                                numBytes,
+                                intent.getStringExtra(MyDownloadService.EXTRA_DOWNLOAD_PATH)));
+                        break;
+                    case MyDownloadService.DOWNLOAD_ERROR:
+                        // Alert failure
+                        showMessageDialog("Error", String.format(Locale.getDefault(),
+                                "Failed to download from %s",
+                                intent.getStringExtra(MyDownloadService.EXTRA_DOWNLOAD_PATH)));
+                        break;
+                    case MyUploadService.UPLOAD_COMPLETED:
+                    case MyUploadService.UPLOAD_ERROR:
+                        onUploadResultIntent(intent);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        // Check if this Activity was launched by clicking on an upload notification
+        if (intent.hasExtra(MyUploadService.EXTRA_DOWNLOAD_URL)) {
+            onUploadResultIntent(intent);
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        updateUI();
+
+        // Register receiver for uploads and downloads
+        LocalBroadcastManager manager = LocalBroadcastManager.getInstance(this);
+        manager.registerReceiver(mBroadcastReceiver, MyDownloadService.getIntentFilter());
+        manager.registerReceiver(mBroadcastReceiver, MyUploadService.getIntentFilter());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // Unregister download receiver
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mBroadcastReceiver);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle out) {
+        super.onSaveInstanceState(out);
+        out.putParcelable(KEY_FILE_URI, mFileUri);
+        out.putParcelable(KEY_DOWNLOAD_URL, mDownloadUrl);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(TAG, "onActivityResult:" + requestCode + ":" + resultCode + ":" + data);
+        if (requestCode == RC_TAKE_PICTURE) {
+            if (resultCode == RESULT_OK) {
+                mFileUri = data.getData();
+
+                if (mFileUri != null) {
+                    uploadFromUri(mFileUri);
+                } else {
+                    Log.w(TAG, "File URI is null");
+                }
+            } else {
+                Toast.makeText(this, "Taking picture failed.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void uploadFromUri(Uri fileUri) {
+        Log.d(TAG, "uploadFromUri:src:" + fileUri.toString());
+
+        // Save the File URI
+        mFileUri = fileUri;
+
+        // Clear the last download, if any
+        updateUI();
+        mDownloadUrl = null;
+
+        // Start MyUploadService to upload the file, so that the file is uploaded
+        // even if this Activity is killed or put in the background
+        startService(new Intent(this, MyUploadService.class)
+                .putExtra(MyUploadService.EXTRA_FILE_URI, fileUri)
+                .setAction(MyUploadService.ACTION_UPLOAD));
+
+        // Show loading spinner
+        showProgressDialog(getString(R.string.progress_uploading));
+    }
+
+    private void beginDownload() {
+        // Get path
+        String path = "photos/" + mFileUri.getLastPathSegment();
+
+        // Kick off MyDownloadService to download the file
+        Intent intent = new Intent(this, MyDownloadService.class)
+                .putExtra(MyDownloadService.EXTRA_DOWNLOAD_PATH, path)
+                .setAction(MyDownloadService.ACTION_DOWNLOAD);
+        startService(intent);
+
+        // Show loading spinner
+        showProgressDialog(getString(R.string.progress_downloading));
+    }
+
+    private void launchCamera() {
+        Log.d(TAG, "launchCamera");
+
+        // Pick an image from storage
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, RC_TAKE_PICTURE);
+    }
+
+
+    private void onUploadResultIntent(Intent intent) {
+        // Got a new intent from MyUploadService with a success or failure
+        mDownloadUrl = intent.getParcelableExtra(MyUploadService.EXTRA_DOWNLOAD_URL);
+        mFileUri = intent.getParcelableExtra(MyUploadService.EXTRA_FILE_URI);
+
+        updateUI();
+    }
+
+    private void updateUI() {
+
+        // Download URL and Download button
+        if (mDownloadUrl != null) {
+            ((TextView) findViewById(R.id.picture_download_uri))
+                    .setText(mDownloadUrl.toString());
+            findViewById(R.id.layout_download).setVisibility(View.VISIBLE);
+        } else {
+            ((TextView) findViewById(R.id.picture_download_uri))
+                    .setText(null);
+            findViewById(R.id.layout_download).setVisibility(View.GONE);
+        }
+    }
+
+    private void showMessageDialog(String title, String message) {
+        AlertDialog ad = new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .create();
+        ad.show();
+    }
+
+    private void showProgressDialog(String caption) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setIndeterminate(true);
+        }
+
+        mProgressDialog.setMessage(caption);
+        mProgressDialog.show();
+    }
+
+    private void hideProgressDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_upload, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int i = item.getItemId();
+        if (i == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            updateUI();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        int i = v.getId();
+        if (i == R.id.chooseImage_button) {
+            launchCamera();
+        }else if (i == R.id.downloadImage_button) {
+            beginDownload();
+        }
+    }
+}
